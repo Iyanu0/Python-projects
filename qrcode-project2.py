7b3b11434e07cc102feafbafff4fdf3350e00b4b
Iyanu0 <unayiokunnuga246@gmail.com>
2024-09-18 21:05:28 +0100

    Added decode and a cuztomised qrcode
    So i learnt about decoding and customizing qrcode

--- qrcode-project2.py
+++ qrcode-project2.py
@@ -1,13 +1,7 @@
 import qrcode
-
 data='Dont forget to subscribe!'
-
 qr=qrcode.QRCode(version=1, box_size=10, border=5)
-
 qr.add_data(data)
-
 qr.make(fit=True)
-
 img=qr.make_image(fill_color='red',back_color='white')
-
 img.save('C:/Users/Iyanu.O/Documents/Python Work/Python-projects/myqrcode2.png')
